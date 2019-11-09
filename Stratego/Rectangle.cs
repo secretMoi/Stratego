c1373ad6994dd818872d51a48b5c086fffd55c5e
winmo <win.moi@hotmail.com>
2019-11-09 12:32:51 +0100

    Flickering battu

---
+++ Stratego/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿namespace Stratego
+{
+    // cette classe permet de maniuler la struct Rectangle de c# grâce à un objet
+    public class Rectangle
+    {
+        private System.Drawing.Rectangle rectangle;
+        public Rectangle(int posX, int posY, int longueur, int hauteur)
+        {
+            rectangle = new System.Drawing.Rectangle(posX, posY, longueur, hauteur);
+        }
+
+        public int X
+        {
+            set { rectangle.X = value; }
+        }
+        public int Y
+        {
+            set { rectangle.Y = value; }
+        }
+        
+        public System.Drawing.Rectangle Rect
+        {
+            get { return rectangle; }
+        }
+    }
+}
